eceb04326f5a167a8fa199f6284222810c3410d1
Sasheem <sts12@me.com>
2018-06-30 14:11:07 -0400

    set up panResponder, all cards now drag across the screen

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import {
+  View,
+  Animated,
+  PanResponder
+} from 'react-native';
 
 class Deck extends Component {
+
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+
+    // defined as a local variable
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (evt, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+    /*
+    1- called anytime the user taps on the screen
+      if we return true, then we want this pan responder to be responsible for that gesture
+      if we return false, then we don't want this to be related to that gesture occuring from user
+    2- a callback called anytinme the user drags their finger across the screen
+    3- called anytime the user removes their finger from the screen
+      good place to do some finalized callback stuff
+    */
+
+    // stay out of the habit of mutating state directly, biting my tongue cause of the official docs
+    this.state = { panResponder, position };
+  }
 
   renderCards() {
     return this.props.data.map(item => {
@@ -11,9 +41,12 @@ class Deck extends Component {
 
   render() {
     return (
-      <View>
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     );
   }
 }
