7e67d536880acc717862b221e5ae42e600de5534
Sasheem <sts12@me.com>
2018-07-02 15:08:52 -0400

    style change

--- App.js
+++ App.js
@@ -69,6 +69,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    top: 50
   },
 });
